6024acdee928b1ac494f59ba614314c96a83c0c4
miguelsaam <110102182+miguelsaam@users.noreply.github.com>
2022-09-29 09:07:42 -0300

    Update Estudo.js

--- Estudo.js
+++ Estudo.js
@@ -1,1 +1,15 @@
+let tabuada = 2;
 
+function tabuada(){
+    document.write(tabuada + " x 1  = " + (tabuada*1)+"<br>");
+    document.write(tabuada + " x 1  = " + (tabuada*1)+"<br>");
+    document.write(tabuada + " x 1  = " + (tabuada*1)+"<br>");
+    document.write(tabuada + " x 1  = " + (tabuada*1)+"<br>");
+    document.write(tabuada + " x 1  = " + (tabuada*1)+"<br>");
+    document.write(tabuada + " x 1  = " + (tabuada*1)+"<br>");
+    document.write(tabuada + " x 1  = " + (tabuada*1)+"<br>");
+    document.write(tabuada + " x 1  = " + (tabuada*1)+"<br>");
+    document.write(tabuada + " x 1  = " + (tabuada*1)+"<br>");
+    document.write(tabuada + " x 1  = " + (tabuada*1)+"<br>");
+
+}
